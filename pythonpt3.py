2c84f30e6870fa1b3d43a486c68e50065c3e1ce7
mathgod27 <robinsonmary01@gmail.com>
2024-08-14 15:47:23 -0700

    new_start

---
+++ pythonpt3.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import matplotlib as plt
+
+months = ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+sales = [50, 40, 50, 45, 40, 35, 40, 45, 55, 65, 95, 90]
+
+plt.plot(months,sales, color = 'purple', linewidth = 4, marker = 'o', markerfacecolor = 'black', markersize = 10)
+
+plt.xlabel('Months of the Year')
+
+plt.ylabel('Sales in Thousands')
+
+plt.title('Janie and Jack Monthly Sales')
+
+plt.show()
